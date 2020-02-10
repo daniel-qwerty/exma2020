582bf81a55fc52ceb29e150b08255e7b2299d9a0
Daniel Monroy <daniel.monroy.b@gmail.com>
2020-02-10 17:43:52 +0000

    nuevo formato en la seccion de sponsrs y se oculataron los speakers, correcciones varias

--- js/home.js
+++ js/home.js
@@ -8,7 +8,11 @@ const app = new Vue({
         mensaje: null,
         showMessage: false,
         message: null,
-        slideCount:0
+        slideCount:0,
+        slideCountS:0,
+        sw: 0,
+        next: null,
+        prev: null
     },
     methods: {
         sendContact(){
@@ -49,12 +53,51 @@ const app = new Vue({
                 console.log(this.slideCount);
             }
             
+        },
+        nextSponsor(){
+            if(this.slideCountS < this.data.sponsors.length-1){
+                this.slideCountS++;
+                console.log(this.slideCountS);
+            }
+            
+        },
+        prevSponsor(){
+            if(this.slideCountS >= 1){
+                this.slideCountS--;
+                console.log(this.slideCountS);
+            }
+            
+        },
+        autoNext() {
+          if (this.sw === 0) {
+            if (this.slideCountS < this.data.sponsors.length-1) {
+              this.slideCountS++;
+
+              //this.logos = this.paginator(this.obj, this.offsetNew).data;
+            } else {
+              this.sw = 1;
+            }
+          } else {
+            if (this.slideCountS >= 1) {
+              this.slideCountS--;
+
+              //this.logos = this.paginator(this.obj, this.offsetNew).data;
+            } else {
+              this.sw = 0;
+            }
+          }
+        },
+        stopCarusel() {
+          clearInterval(this.next);
+        },
+        startCarusel() {
+          this.next = setInterval(() => this.autoNext(), 6000);
         }
     },
     mounted() { 
         axios.get("data.json").then(response => {
             this.data = (response.data);
-            console.log(this.data.titulo);
+            this.next = setInterval(() => this.autoNext(), 6000);
         });
 
          
