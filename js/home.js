1f0597100ca94c3576fb30f45228cfd9208c95ec
Daniel Monroy <daniel.monroy.b@gmail.com>
2020-02-10 00:43:48 +0000

    cargar data desde json y pluggin chat facebook

--- js/home.js
+++ js/home.js
@@ -1,16 +1,63 @@
 const app = new Vue({
     el: "#app",
     data: {
-        data: Object
+        data: Object,
+        nombre: null,
+        apellido: null,
+        email: null,
+        mensaje: null,
+        showMessage: false,
+        message: null,
+        slideCount:0
     },
-    methods: {},
-    mounted() {
-       /* if (!sessionStorage.getItem("login")) {
-            window.location.href = "/memorias-cx-2019/";
+    methods: {
+        sendContact(){
+            if(this.nombre != null && this.apellido != null && this.email != null && this.mensaje != null){
+                axios
+                .post('http://localhost:8080/api-rest/public/api/clientes/nuevo', {
+                  nombre: this.nombre,
+                  apellido: this.apellido,
+                  email: this.email,
+                  mensaje: this.mensaje,
+                  web: "2020"
+                })
+                .then(res => {
+                    this.showMessage = true;
+                    this.message = "Su mensaje fue enviado";
+                })
+                .catch(function (error) {
+                  console.log(error);
+                  this.showMessage = true;
+                  this.message = error;
+                });
+            } else {
+                this.showMessage = true;
+                this.message = "Todos los campos del formulario son obligatoiros";
+            }
+            
+        },
+        nextSlide(){
+            if(this.slideCount < this.data.slidesExperience.length-1){
+                this.slideCount++;
+                console.log(this.slideCount);
+            }
+            
+        },
+        prevSlide(){
+            if(this.slideCount >= 1){
+                this.slideCount--;
+                console.log(this.slideCount);
+            }
+            
         }
-        sessionStorage.setItem("encuesta", "yes");
-        axios.get("/data.json").then(response => {
-            this.data = response.data;
-        });*/
+    },
+    mounted() { 
+        axios.get("data.json").then(response => {
+            this.data = (response.data);
+            console.log(this.data.titulo);
+        });
+
+         
+          
     }
 });
