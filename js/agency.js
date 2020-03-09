22eef516e08fb87168ce699eaacbc9801893b020
Daniel Monroy <daniel.monroy.b@gmail.com>
2020-03-09 21:04:04 +0000

    scroll to tickets al entrar por url directa, ejemplo https://exma.com.bo/#tickets 6

--- js/agency.js
+++ js/agency.js
@@ -27,7 +27,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  /*var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -37,6 +37,6 @@
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  $(window).scroll(navbarCollapse);*/
 
 })(jQuery); // End of use strict
